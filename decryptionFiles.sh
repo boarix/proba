f7a8ed1578a797bdec5e15c8b07f696dfc7ebef3
boarix <jakub_dziegielewski@o2.pl>
2016-11-23 22:47:40 +0100

    Update decryptionFiles.sh

--- decryptionFiles.sh
+++ decryptionFiles.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+i = 0
 while read line           
 do           
-    echo "$line"       
+    $i = $i + 1
+    files[$i] "$line"
 done <./encryptedFiles.txt
+for s in $files
+do
+   echo "$s"
+done
