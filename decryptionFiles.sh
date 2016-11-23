62237d475afb300d101c5de45e6d28af794a1bcd
boarix <jakub_dziegielewski@o2.pl>
2016-11-23 23:39:40 +0100

    Update decryptionFiles.sh

--- decryptionFiles.sh
+++ decryptionFiles.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 while read line           
-do           
-    openssl aes-256-cbc -d -in $line -k $1
+do         
+    if [ "$line" != "" ]
+    then
+        openssl aes-256-cbc -d -in $line -k $1
+    fi
 done <./encryptedFiles.txt
