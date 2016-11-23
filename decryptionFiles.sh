07fe57e695f587e9374888b54eafd88c70201af1
boarix <jakub_dziegielewski@o2.pl>
2016-11-24 00:18:18 +0100

    Update decryptionFiles.sh

--- decryptionFiles.sh
+++ decryptionFiles.sh
@@ -3,6 +3,7 @@
 while read line           
 do         
     echo "POKAZ CO SIE DZIEJE!!!!"
+    echo $line
     if [ "$line" != "" ]
     then
         openssl aes-256-cbc -d -in $line -k $1 > $line
