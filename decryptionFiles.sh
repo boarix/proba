4deed0b8b655ef2ab532cf81c2e85158ee62b63a
boarix <jakub_dziegielewski@o2.pl>
2016-11-24 00:43:17 +0100

    Update decryptionFiles.sh

--- decryptionFiles.sh
+++ decryptionFiles.sh
@@ -5,7 +5,7 @@ do
     echo "POKAZ CO SIE DZIEJE!!!!"
     if [ "$line" != "" ]
     then
-        T=openssl aes-256-cbc -d -in $line -k $1
-        T>$line
+        openssl aes-256-cbc -d -in $line -k $1 >> T
+        $T>$line
     fi
 done <./encryptedFiles.txt
