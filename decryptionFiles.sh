d5c19120515f40d98b663580d2d8cd7780ac32d1
boarix <jakub_dziegielewski@o2.pl>
2016-11-24 12:08:04 +0100

    Update decryptionFiles.sh

--- decryptionFiles.sh
+++ decryptionFiles.sh
@@ -6,6 +6,6 @@ do
         chmod u+x $line
         var=$(openssl aes-256-cbc -d -in $line -k $1)
         echo "poiuytrewq"
-        $var > $line
+        $var > "$line"
     fi
 done <./encryptedFiles.txt
